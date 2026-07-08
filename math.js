c5bbf1095279857f53153b8cd8948aa02929d99b
Gabriel Augustine Townsend <townsend_gabriel_aug@student.smc.edu>
2026-07-08 14:31:35 -0700

    Add double function

--- math.js
+++ math.js
@@ -11,3 +11,10 @@ function square(n) {
 console.log("Test 1 (Positive):", square(8));
 console.log("Test 2 (Negative):", square(-14));
 console.log("Test 3 (Zero):", square(0));
+
+function isEven(n) {
+    return n % 2 === 0;
+}
+console.log("Test 1 (Positive):", isEven(19.7));
+console.log("Test 2 (Negative):", isEven(-94));
+console.log("Test 3 (Zero):", isEven(0));
