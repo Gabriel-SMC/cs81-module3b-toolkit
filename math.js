9686d6e8f6cdf6acc43d7defa018e24ae3bd3b9b
Gabriel Augustine Townsend <townsend_gabriel_aug@student.smc.edu>
2026-07-08 18:09:13 -0700

    Add double function

--- math.js
+++ math.js
@@ -19,9 +19,27 @@ console.log("Test 1 (Positive):", isEven(19.7));
 console.log("Test 2 (Negative):", isEven(-94));
 console.log("Test 3 (Zero):", isEven(0));
 
-function isOdd(n){
+function isOdd(n) {
     return n % 2 !== 0;
 }
-console.log("Test 1 (Positive):", isOdd(12,6));
-console.log("Test 2 (Negative):", isOdd(-76));
+console.log("Test 1 (Positive):", isOdd(12.6));
+console.log("Test 2 (Negative):", isOdd(-76.52));
 console.log("Test 3: Zero:", isOdd(0));
+
+function multiply(n) {
+  let product = 1;
+  for (let i = 1; i <= n; i++) {
+    product *= i;
+  }
+  return product;
+}
+console.log("Test 1 (Positive):", multiply(7));
+console.log("Test 2 (Negative):", multiply(-16.4));
+console.log("Test 3 (Zero):", multiply(0));
+
+function addition(n) {
+    return n + 5;
+}
+console.log("Test 1(Positive):", addition(14));
+console.log("Test 2 (Negative):", addition(-9));
+console.log("Test 3 (Zero)", addition(0));
